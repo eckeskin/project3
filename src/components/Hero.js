c4b8526fe3372ce4070a99725dae742afa8e440c
Eyuep Keskin <eyuep.keskin@hst.de>
2023-03-02 21:24:54 +0100

    commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero(){
     return(
        <section className="hero">
             <img src={heroImg} className="hero-img" alt="hero-img"/>
-            <h1 className="hero-header">Online Experiences</h1>
+            <h1 className="hero-header">Online Experiences - Eyüp Caglar Keskin</h1>
             <p className="hero-text">Join unique interactive activities led by one-of-a-kind 
             hosts—all without leaving home.</p>
 
